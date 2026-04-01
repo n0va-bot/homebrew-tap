6fd2de9c3fccd84ead3b0afdc7b74008f82bb98b
N0VA <N0VA@users.noreply.github.com>
2026-04-01 07:05:01 +0000

    Update kewt to v1.5.3

--- Formula/kewt.rb
+++ Formula/kewt.rb
@@ -1,10 +1,10 @@
 class Kewt < Formula
   desc "Minimalist static site generator inspired by werc"
   homepage "https://kewt.krzak.org"
-  url "https://github.com/n0va-bot/kewt/releases/download/v1.5.2/kewt"
-  sha256 "122098d4ebc05c541942d612ad4c3b50e2e17f5550f92b18933b1c27487c414d"
+  url "https://github.com/n0va-bot/kewt/releases/download/v1.5.3/kewt"
+  sha256 "2224340603c477d5934913c1ecfb507595d446534e1cdf519d7b63803bb29837"
   license "ISC"
-  version "1.5.2"
+  version "1.5.3"
 
   def install
     bin.install "kewt"
