ba53bcfdfb35eefe7434519194274924c8aab2c8
N0VA <N0VA@users.noreply.github.com>
2026-05-20 08:35:08 +0000

    Update kewt to v1.6.7

--- Formula/kewt.rb
+++ Formula/kewt.rb
@@ -1,15 +1,15 @@
 class Kewt < Formula
   desc "Minimalist static site generator inspired by werc"
   homepage "https://kewt.krzak.org"
-  url "https://github.com/n0va-bot/kewt/releases/download/v1.6.6/kewt"
-  sha256 "d9bf6bd6d7290e75dae2d7c08869549dda7c3e620e8ea1c8be94fa0d1487c954"
+  url "https://github.com/n0va-bot/kewt/releases/download/v1.6.7/kewt"
+  sha256 "92326e979aa3d58441842b43f9fa9a98abda0f06fe08a8331ef16b9516cdddc7"
   license "ISC"
-  version "1.6.6"
+  version "1.6.7"
 
   def install
     bin.install "kewt"
     chmod 0755, bin/"kewt"
-    generate_completions_from_executable(bin/"kewt", "--dump-zsh-completions", shells: [:zsh])
+    generate_completions_from_executable(bin/"kewt", "--dump-zsh-completions", shells: [:zsh, :bash])
   end
 
   test do
